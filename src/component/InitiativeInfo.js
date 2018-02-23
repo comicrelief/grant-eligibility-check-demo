c642af8f1fb87b364a2c9105eacc5b853a92dff7
Alex <alex@recordsonribs.com>
2018-02-23 15:36:05 +0000

    Make things a bit nicer

--- src/component/InitiativeInfo.js
+++ src/component/InitiativeInfo.js
@@ -9,38 +9,43 @@ export default class InitiativeInfo extends Component {
     }
 
     return (
-      <ul>
-        <li><strong>Name:</strong> {initiative.name}</li>
-        <li><strong>Grants range:</strong>&nbsp;
-          {initiative.project_funding_min.amount} {initiative.project_funding_min.currency} &ndash;&nbsp;
-          {initiative.project_funding_max.amount} {initiative.project_funding_max.currency}
-        </li>
-        <li><strong>Countries:</strong>
+      <div style={{ width: '700px'}}>
+        <h2>{initiative.name}</h2>
+        <p>{initiative.description}</p>
+        <h3>Grants range</h3>
+        <p>We typically give between {initiative.project_funding_min.amount} {initiative.project_funding_min.currency} and  {initiative.project_funding_max.amount} {initiative.project_funding_max.currency} to grants in the {initiative.name} initatives.</p>
+        <h3>Countries</h3>
+        <p>
           <ul>
               {
                 initiative.eligible_organisation_countries.map((country) => <li>{country}</li>)
               }
           </ul>
-        </li>
-        <li><strong>Common mistakes:</strong>
+        </p>
+        <h3>Common mistakes</h3>
+        <p>
           <ul>
             {
               initiative.common_mistakes.map((mistake) => <li>{mistake.summary}</li>)
             }
           </ul>
-        </li>
-        <li><strong>Good ideas:</strong>
+        </p>
+        <h3>Good ideas</h3>
+        <p>
           <ul>
             {
               initiative.good_stuff.map((goodThing) => <li>{goodThing.summary}</li>)
             }
           </ul>
-        </li>
-        <li><strong>Success rate for last year:</strong>&nbsp;
+        </p>
+        <h3>
+          Success rate for last year
+        </h3>
+        <p>
           {initiative.past_acceptances[0].accepted_percent}% &ndash; that's&nbsp;
             {initiative.past_acceptances[0].accepted_number} applications.
-        </li>
-      </ul>
+        </p>
+      </div>
     );
   }
 }
