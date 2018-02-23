6224e8e9977e12f9681c7864b0337ace1d5006cf
Noel Light-Hilary <noel@noellh.com>
2018-02-23 14:57:19 +0000

    More initiative info

--- src/component/InitiativeInfo.js
+++ src/component/InitiativeInfo.js
@@ -10,17 +10,35 @@ export default class InitiativeInfo extends Component {
 
     return (
       <ul>
-        <li><strong>Name</strong>: {initiative.name}</li>
-        <li><strong>Grants range</strong>:&nbsp;
+        <li><strong>Name:</strong> {initiative.name}</li>
+        <li><strong>Grants range:</strong>&nbsp;
           {initiative.project_funding_min.amount} {initiative.project_funding_min.currency} &ndash;&nbsp;
           {initiative.project_funding_max.amount} {initiative.project_funding_max.currency}
         </li>
-        <li><strong>Countries</strong>:
+        <li><strong>Countries:</strong>
           <ul>
               {
                 initiative.eligible_organisation_countries.map((country) => <li>{country}</li>)
               }
           </ul>
+        </li>
+        <li><strong>Common mistakes:</strong>
+          <ul>
+            {
+              initiative.common_mistakes.map((mistake) => <li>{mistake.summary}</li>)
+            }
+          </ul>
+        </li>
+        <li><strong>Good ideas:</strong>
+          <ul>
+            {
+              initiative.good_stuff.map((goodThing) => <li>{goodThing.summary}</li>)
+            }
+          </ul>
+        </li>
+        <li><strong>Success rate for last year:</strong>&nbsp;
+          {initiative.past_acceptances[0].accepted_percent}% &ndash; that's&nbsp;
+            {initiative.past_acceptances[0].accepted_number} applications.
         </li>
       </ul>
     );
