d3839466cc84d9d9f2424678c231908421e7bfa6
Alex <alex@recordsonribs.com>
2018-02-23 14:47:36 +0000

    Make things a little nicer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Machine, State } from "react-gizmo";
+import logo from './logo.svg';
+
+const NotMachine = () => (
+    <React.Fragment>
+        <header role="banner" style={{
+            marginBottom: '40px',
+            display: 'flex',
+            justifyContent: 'center',
+        }}>
+            <div className="header">
+                <a href="https://www.comicrelief.com/" rel="home" className="header-logo">
+                <img 
+                    src={logo}
+                    style={{
+                        height: '40px',
+                        marginTop: '20px',
+                        marginBottom: '20px',
+                    }}
+                />
+                <span className="visually-hidden">Go to Home Page</span>
+                </a>
+            </div>
+        </header>
+        <main style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}>
+        <h1 className="text-align-center">
+            Apply for grant
+        </h1>
+        <p className="text-align-center font--xlarge">
+            Are you a UK based charity?
+        </p>
+        <p style={{ width: '700px'}}>
+            For projects in the UK, we fund work in England, Northern Ireland, Scotland, Wales, the Channel Islands and the Isle of Man. Depending on the funding initiative, Comic Relief may also welcome proposals from organisations registered outside of the UK.
+        </p>
+        <div style={{display: 'flex'}}>
+            <button className="btn btn--red" href="#">Yes</button>
+            <button className="btn btn--red">
+                No
+            </button>
+        </div>
+        </main>
+    </React.Fragment>
+)
 
 const state = {
   initialState: { text: "Hello" },
@@ -73,4 +119,4 @@ const MachineApp = () => (
   </Machine>
 );
 
-ReactDOM.render(<MachineApp />, document.getElementById("root"));
+ReactDOM.render(<NotMachine />, document.getElementById("root"));
